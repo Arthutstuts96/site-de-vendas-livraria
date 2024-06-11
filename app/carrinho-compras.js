ea32b105bab66542fd1d5f98ece2ca931670c6d3
Arthutstuts96 <oarthurlima1575@gmail.com>
2024-06-11 14:40:26 -0300

    Mockando api com json-server

---
+++ app/carrinho-compras.js
@@ -0,0 +1,6 @@
+const carrinho = document.getElementById('carrinho-de-compras');
+const livros = document.querySelectorAll('.livro');
+
+carrinho.addEventListener('click', function(){
+    console.log('carrinho aberto');
+});
--- app/main.js
+++ app/main.js
@@ -1,18 +1,29 @@
 const listaLivros = document.querySelector('.lista__livros');
 
-// pegarLivrosNaApi();
+pegarLivrosNaApi();
 /******************** */
 async function pegarLivrosNaApi(){
-    const api = await fetch('http://localhost:3000/livros');
-    const resposta = await api.json();
-
-    resposta.forEach((livro) => {
-        listaLivros.innerHTML += `
-            <li>
-                <p>${livro.nome}</p>
-                <p>${livro.autor}</p>
-                <p>${livro.preco}</p>
-            </li>
+    const url = 'http://localhost:3000/livros';
+    try {
+        const api = await fetch(url);
+        const resposta = await api.json();
+    
+        resposta.forEach((livro) => {
+            listaLivros.innerHTML += `
+                <li class="livro">
+                    <button class="adicionar-carrinho">+</button>
+                    <img src=${livro.imagem} alt="Imagem da capa do livro" width="225" height="300">
+                    <p class="titulo__livro">${livro.nome}</p>
+                    <div class="descricao__livro">
+                        <p class="autor__livro">${livro.autor}</p>
+                        <p class="preco__livro">R$${livro.preco}</p>
+                    </div>
+                </li>
+            `
+        });
+    }catch(error){
+        listaLivros.innerHTML = `
+            <span>Erro: Livros não encontrados. Mais informações: ${error}</span>
         `
-    });
+    }
 }
